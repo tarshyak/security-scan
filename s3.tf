efad03be45a0e649ace5d17ca58da516fc219e86
tarshyak <115622018+tarshyak@users.noreply.github.com>
2024-03-22 13:53:36 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1" # Choose your AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "bucket6961" # Ensure this is unique
+  acl    = "private" # Defines the access control level
+
+  tags = {
+    Name        = "My Terraform S3 Bucket"
+    Environment = "Dev"
+  }
+}
